07362514006786c48ca3ae38da5c44dc52b8256a
Lucassderezende <92000780+Lucassderezende@users.noreply.github.com>
2025-12-08 19:09:46 -0300

    feat: adiciona template index.html e lista alimentos na página inicial

--- contador_de_calorias/views.py
+++ contador_de_calorias/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Alimentos
 
+def index(request):
+    alimento = Alimentos.objects.all()
+    return render(request, "contador_de_calorias/index.html", {"alimento":alimento})
 
