83e00271615cfefdef904dc86c5e3b010150f280
Lucassderezende <92000780+Lucassderezende@users.noreply.github.com>
2025-12-16 15:20:05 -0300

    feat: lista alimentos ingeridos pelo usuário no index

--- contador_de_calorias/views.py
+++ contador_de_calorias/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import render
 from .models import Alimentos, AlimentoIngerido
 from django.shortcuts import get_object_or_404
-from django.contrib import messages
 
 def index(request):
 
-    if request.method == "POST":
-        alimento_ingerido = request.POST["alimento_consumido"]
-        alimento = get_object_or_404(Alimentos, nome=alimento_ingerido)
-        user = request.user
-        registro = AlimentoIngerido(user=user, alimento_ingerido=alimento)
-        registro.save()
         alimentos = Alimentos.objects.all()
-        return render(request, "contador_de_calorias/index.html", {"alimentos":alimentos})
-    else:
-        alimentos = Alimentos.objects.all()
-        return render(request, "contador_de_calorias/index.html", {"alimentos":alimentos})
 
-
+        if request.method == "POST":
+            alimento_ingerido = request.POST["alimento_consumido"]
+            alimento = get_object_or_404(Alimentos, nome=alimento_ingerido)
+            registro = AlimentoIngerido(user=request.user, alimento_ingerido=alimento)
+            registro.save()
+        alimentos_registrados = AlimentoIngerido.objects.filter(user=request.user)
+        return render(request, "contador_de_calorias/index.html", {"alimentos":alimentos, "alimentos_registrados":alimentos_registrados})
