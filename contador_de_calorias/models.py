80465ccc72150794659cc31c82a62b72b214cc8a
Lucassderezende <92000780+Lucassderezende@users.noreply.github.com>
2025-12-16 12:58:40 -0300

    feat: implementa registro de alimento ingerido com POST no index

--- contador_de_calorias/models.py
+++ contador_de_calorias/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 
 class Alimentos(models.Model):
 
-    def __str__(self):
-        return self.nome
-    
     nome = models.CharField(max_length=120)
     carboidratos = models.FloatField()
     proteinas = models.FloatField()
     gorduras = models.FloatField()
     calorias = models.IntegerField()
 
+    def __str__(self):
+        return self.nome
 
 class AlimentoIngerido(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     alimento_ingerido = models.ForeignKey(Alimentos, on_delete=models.CASCADE)
+
 
--- contador_de_calorias/views.py
+++ contador_de_calorias/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from .models import Alimentos
+from .models import Alimentos, AlimentoIngerido
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
 
 def index(request):
-    alimento = Alimentos.objects.all()
-    return render(request, "contador_de_calorias/index.html", {"alimento":alimento})
+
+    if request.method == "POST":
+        alimento_ingerido = request.POST["alimento_consumido"]
+        alimento = get_object_or_404(Alimentos, nome=alimento_ingerido)
+        user = request.user
+        registro = AlimentoIngerido(user=user, alimento_ingerido=alimento)
+        registro.save()
+        alimentos = Alimentos.objects.all()
+        return render(request, "contador_de_calorias/index.html", {"alimentos":alimentos})
+    else:
+        alimentos = Alimentos.objects.all()
+        return render(request, "contador_de_calorias/index.html", {"alimentos":alimentos})
+
 
