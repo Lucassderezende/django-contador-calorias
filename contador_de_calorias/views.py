9905ff3d4174d33fb9bd28939440233571edda05
Lucassderezende <92000780+Lucassderezende@users.noreply.github.com>
2025-12-20 20:33:25 -0300

    refactor: calcula totais e porcentagens no backend

--- contador_de_calorias/views.py
+++ contador_de_calorias/views.py
@@ -2,17 +2,64 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Alimentos, AlimentoIngerido
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
 
     alimentos = Alimentos.objects.all()
 
     if request.method == "POST":
-        alimento_ingerido = request.POST["alimento_consumido"]
-        alimento = get_object_or_404(Alimentos, nome=alimento_ingerido)
-        registro = AlimentoIngerido(user=request.user, alimento_ingerido=alimento)
-        registro.save()
+        alimento_nome = request.POST["alimento_consumido"]
+        alimento = get_object_or_404(Alimentos, nome=alimento_nome)
+        AlimentoIngerido.objects.create(
+            user = request.user,
+            alimento_ingerido = alimento
+        )
+
     alimentos_registrados = AlimentoIngerido.objects.filter(user=request.user)
-    return render(request, "contador_de_calorias/index.html", {"alimentos":alimentos, "alimentos_registrados":alimentos_registrados})
+
+    total_carboidratos = sum(
+       a.alimento_ingerido.carboidratos for a in alimentos_registrados
+    )
+
+    total_proteinas = sum(
+        a.alimento_ingerido.proteinas for a in alimentos_registrados
+    )
+
+    total_gorduras = sum(
+        a.alimento_ingerido.gorduras for a in alimentos_registrados
+    )
+
+    total_calorias = sum(
+        a.alimento_ingerido.calorias for a in alimentos_registrados
+    )
+
+    kcalCarb = total_carboidratos * 4;
+    kcalProt = total_proteinas * 4;
+    kcalGord = total_gorduras * 9;
+
+    totalKcal = kcalCarb + kcalProt + kcalGord;
+
+    porcCarb = round((kcalCarb / totalKcal) * 100, 2) if totalKcal else 0
+    porcProt = round((kcalProt / totalKcal) * 100, 2) if totalKcal else 0
+    porcGord  = round((kcalGord / totalKcal) * 100, 2) if totalKcal else 0
+
+    contexto = {
+        "alimentos": alimentos,
+        "alimentos_registrados": alimentos_registrados,
+        "total_carboidratos": round(total_carboidratos, 2),
+        "total_proteinas": round(total_proteinas, 2),
+        "total_gorduras": round(total_gorduras, 2),
+        "total_calorias": total_calorias,
+        "kcalCarb": kcalCarb,
+        "kcalProt": kcalProt,
+        "kcalGord": kcalGord,
+        "totalKcal": totalKcal,
+        "porcCarb": porcCarb,
+        "porcProt": porcProt,
+        "porcGord": porcGord
+    }
+
+    return render(request, "contador_de_calorias/index.html", contexto)
 
 @login_required
 def deletar_consumidos(request, id):
